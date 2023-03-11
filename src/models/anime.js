bb866be2da0eed5e2edcb8a06259a97be424cbb4
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2023-03-11 16:15:12 -0500

    Esquema de Mongoose

--- src/models/anime.js
+++ src/models/anime.js
@@ -1,5 +1,16 @@
 const mongoose = require("mongoose");
 
+
+/**
+ * Esquema de Mongoose para los animes.
+ *
+ * @typedef {Object} AnimeSchema
+ * @property {String} title - El título del anime.
+ * @property {String} writer - El nombre del escritor del anime.
+ * @property {Number} year - El año de lanzamiento del anime.
+ * @property {String} synopsis - Una breve descripción del anime.
+ */
+
 const animeSchema = mongoose.Schema({
     title: {
         type: String,
