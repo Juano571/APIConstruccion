767cfbd3863020a7303d660bf0bbc9ab535101fc
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2023-03-11 16:00:56 -0500

    Jsdocs anime.js

--- src/routes/anime.js
+++ src/routes/anime.js
@@ -3,7 +3,13 @@ const animeSchema = require("../models/anime");
 
 const router = express.Router();
 
-//Add movie
+/**
+ * Crea un nuevo anime en la base de datos.
+ *
+ * @param {Object} req.body - Un objeto que contiene los datos del anime a agregar.
+ * @returns {Object} - El objeto del anime creado.
+ * @throws {Error} - Si ocurre un error al intentar agregar el anime.
+ */
 router.post("/animes", (req, res) => {
     const anime = animeSchema(req.body);
     anime
@@ -12,7 +18,12 @@ router.post("/animes", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
-//Get all
+/**
+ * Obtiene una lista de todos los animes almacenados en la base de datos.
+ *
+ * @returns {Array} - Un array de objetos que representan los animes almacenados.
+ * @throws {Error} - Si ocurre un error al intentar obtener los animes.
+ */
 router.get("/animes", (req, res) => {
     animeSchema
         .find()
@@ -20,7 +31,13 @@ router.get("/animes", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
-//Get one
+/**
+ * Obtiene un anime específico de la base de datos según su ID.
+ *
+ * @param {String} req.params.id - El ID del anime que se desea obtener.
+ * @returns {Object} - El objeto del anime solicitado.
+ * @throws {Error} - Si ocurre un error al intentar obtener el anime.
+ */
 router.get("/animes/:id", (req, res) => {
     const { id } = req.params;
     animeSchema
@@ -29,7 +46,14 @@ router.get("/animes/:id", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
-//Update one
+/**
+ * Actualiza un anime específico en la base de datos según su ID.
+ *
+ * @param {String} req.params.id - El ID del anime que se desea actualizar.
+ * @param {Object} req.body - Un objeto que contiene los datos actualizados del anime.
+ * @returns {Object} - El objeto del anime actualizado.
+ * @throws {Error} - Si ocurre un error al intentar actualizar el anime.
+ */
 router.put("/animes/:id", (req, res) => {
     const { id } = req.params;
     const { title, writer, year, synopsis } = req.body;
@@ -39,7 +63,13 @@ router.put("/animes/:id", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
-//Delete one
+/**
+ * Elimina un anime específico de la base de datos según su ID.
+ *
+ * @param {String} req.params.id - El ID del anime que se desea eliminar.
+ * @returns {Object} - El objeto del anime eliminado.
+ * @throws {Error} - Si ocurre un error al intentar eliminar el anime.
+ */
 router.delete("/animes/:id", (req, res) => {
     const { id } = req.params;
     animeSchema
